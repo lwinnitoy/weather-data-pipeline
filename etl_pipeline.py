d6b210815312ccc5df3fbefc81fcd8da573cfd11
lwinnitoy <137258417+lwinnitoy@users.noreply.github.com>
2025-09-03 12:33:50 -0700

    more github action fixes

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -44,11 +44,11 @@ def load(df):
 
 
     # Fetch variables from github secrets
-    USER = os.getenv("DB_USER")
-    PASSWORD = os.getenv("DB_PASSWORD")
-    HOST = os.getenv("DB_HOST")
-    PORT = os.getenv("DB_PORT")
-    DBNAME = os.getenv("DB_NAME")
+    USER = os.getenv("USER")
+    PASSWORD = os.getenv("PASSWORD")
+    HOST = os.getenv("HOST")
+    PORT = os.getenv("PORT")
+    DBNAME = os.getenv("DBNAME")
 
     # Prepare DataFrame as CSV in-memory
     cols = list(df.columns)
