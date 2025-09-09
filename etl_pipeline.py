a47e5de20684e81464a776a332e99054667097d4
lwinnitoy <137258417+lwinnitoy@users.noreply.github.com>
2025-09-09 00:19:49 -0700

    Added wind speed and pressure data. optimized timestamps

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timezone
+import zoneinfo
 import psycopg2
 from io import StringIO
 from dotenv import load_dotenv
@@ -9,7 +10,7 @@ import os
 
 #comment the line below loads env vars from .env file
 #comment it out to load env variables from github secrets
-#load_dotenv()
+load_dotenv()
 
 # loading openweathermap variables from github secrets
 API_KEY = os.getenv("API_KEY")
@@ -26,15 +27,28 @@ DBNAME = os.getenv("DB_NAME")
 
 def extract():
     url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    data = response.json()
-    return data
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        #catches api errors, if the api returns a non-200 status code
+        if response.status_code != 200:
+            print("API Error. Failed to connect:", data)  # Log the whole response for debugging
+            return None
+        return data
+    except Exception as e:
+        #catches connection errors, timeouts, etc
+        print("Error occurred connecting to openweathermap API:", e)
+        return None
+
+
 
 def transform(data):
 
     #catch api errors
+    #if this error is hit, the api returned incorrect data (data without 'main' key)
     if "main" not in data:
-        print("API Error:", data)  # Log the whole response for debugging
+        print("API Error. Bad response:", data)  # Log the whole response for debugging
         return None
      
     # Extract relevant data
@@ -42,15 +56,22 @@ def transform(data):
     weather_data = data['weather'][0]
     wind_data = data['wind']
 
+     # explicit timezone for Victoria, BC (Pacific Time)
+    local_tz = zoneinfo.ZoneInfo("America/Vancouver")
+
+    now_utc = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")  # UTC timestamp
+    now_local = datetime.now(local_tz).strftime("%Y-%m-%d %H:%M:%S")  # formatted local timestamp
+
     # Create a DataFrame
     df = pd.DataFrame({
         'city': [CITY],
         'temperature_c': [main_data['temp']],
-        #'pressure': [main_data['pressure']],
+        'pressure_hPa': [main_data['pressure']],
         'humidity_percent': [main_data['humidity']],
         'weather_description': [weather_data['description']],
-        #'wind_speed': [wind_data['speed']],
-        'timestamp_utc': [datetime.now()]
+        'wind_speed_kmph': [wind_data['speed'] * 3.6],  # Convert m/s to km/h
+        'created_at_local': [now_local],
+        'created_at': [now_utc]
     })
 
     return df
@@ -112,10 +133,17 @@ def load(df):
             pass
         raise
 
+
+
 def main():
     raw_data = extract()
+    if raw_data is None:
+        print("Extraction failed. Exiting.")
+        return
     df = transform(raw_data)
     load(df)
+
+
 
 
 if __name__ == "__main__":
@@ -134,3 +162,7 @@ if __name__ == "__main__":
         except Exception:
             pass
         raise
+
+
+
+    
